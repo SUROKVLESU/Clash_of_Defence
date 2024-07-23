ecd9eaf71d0b8e40633d76a7414bba894e2cd7ed
SUROKVLESU <surokwnorke@gmail.com>
2024-07-23 21:45:22 +0300

    23.07.2024
    Первая версия колоды. Ее можно скролить влево и вправо. Выбранную карту можно перетащить на экран.

---
+++ Clash_of_Defence/Assets/Test/TestButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TestButton : MonoBehaviour
+{
+    private Button button;
+    [SerializeField] ListBuildingsHand ListBuildingsHand;
+    [SerializeField] GameObject Obj;
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => { ListBuildingsHand.AddCard(Obj); });
+    }
+}
+
