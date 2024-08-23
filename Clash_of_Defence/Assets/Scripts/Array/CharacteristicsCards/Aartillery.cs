1ce6b96ce1342cec2d2e533aebadc0e8a0b971ba
SUROKVLESU <surokwnorke@gmail.com>
2024-08-23 21:26:37 +0300

    23.08.2024
    Сделана артиллерия.

---
+++ Clash_of_Defence/Assets/Scripts/Array/CharacteristicsCards/Aartillery.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Aartillery:AttackingBuildingCharacteristics
+{
+    [SerializeField] protected float SqrProjectileVelocity;
+    protected override IEnumerator AttackCoroutine()
+    {
+        while (true)
+        {
+            if (TransformAttackTarget == null)
+            {
+                Coroutine = StartCoroutine(AimingTargetCoroutine());
+                yield break;
+            }
+            yield return new WaitForSeconds(AttackReloading);
+            if (GameController.instance.IsPause) { yield break; }
+            if (TransformAttackTarget == null) continue;
+            AudioSource.PlayOneShot(AudioSource.clip);
+            Animator.Play(AnimationName);
+            StartCoroutine(AartilleryAttackCoroutine());
+        }
+    }
+    protected virtual IEnumerator AartilleryAttackCoroutine()
+    {
+        yield return new WaitForSeconds((TransformAttackTarget.position-transform.position).sqrMagnitude/SqrProjectileVelocity);
+        if(TransformAttackTarget == null) yield break;
+        AttackTarget.TakingDamage(Damage);
+        if (AttackTarget.GetHP()<0)
+        {
+            StopCoroutine(Coroutine);
+            TransformAttackTarget = null;
+            AttackTarget = null;
+            ActivationBuildings();
+        }
+    }
+}
+
